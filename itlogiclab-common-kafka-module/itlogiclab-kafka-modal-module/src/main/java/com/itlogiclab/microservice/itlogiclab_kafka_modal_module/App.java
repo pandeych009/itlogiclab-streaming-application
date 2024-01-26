c708c1249cdd5d18c078ed256241dff8bbee2564
Chandan Pandey <chandan.pandey83@gmail.com>
2024-01-26 21:19:12 +0530

    Initial commit

---
+++ itlogiclab-common-kafka-module/itlogiclab-kafka-modal-module/src/main/java/com/itlogiclab/microservice/itlogiclab_kafka_modal_module/App.java
@@ -0,0 +1,13 @@
+package com.itlogiclab.microservice.itlogiclab_kafka_modal_module;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ itlogiclab-streaming-consumer-application/src/main/java/com/itlogiclab/microservice/itlogiclab_streaming_consumer_application/App.java
@@ -0,0 +1,13 @@
+package com.itlogiclab.microservice.itlogiclab_streaming_consumer_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
